655a54aee2f5fa92a5147ed41bfd58e7e7191b00
frostmoor <67586831+frostmoor@users.noreply.github.com>
2024-12-03 21:04:41 +0900

    기억력 카드게임 만들기

--- memory_card_game.py
+++ memory_card_game.py
@@ -3,13 +3,13 @@ import random
 import time
 import os
 
-# Pygame 초기화
+# 초기화
 pygame.init()
 
 # 화면 크기
 WIDTH, HEIGHT = 600, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Memory Matching Game')
+pygame.display.set_caption('메모리 카드 게임')
 
 # 칼라
 WHITE = (255, 255, 255)
@@ -50,6 +50,7 @@ def main():
     revealed = [False] * 10
     first_card = None
     matches_found = 0
+    can_click = True  # 클릭 가능 여부 플래그
 
     runChk = True
     while runChk:
@@ -57,30 +58,39 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 runChk = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN and can_click:
                 x, y = event.pos
                 card_index = (y // (CARD_HEIGHT + MARGIN)) * 5 + (x // (CARD_WIDTH + MARGIN))
                 if not revealed[card_index]:
                     revealed[card_index] = True
+                    drawBoard(deck, revealed)
                     if first_card is None:
                         first_card = card_index
                     else:
+                        can_click = False  # 두 번째 카드 선택 후 클릭 방지 플래그
                         if check_match(deck, first_card, card_index):
                             matches_found += 1
+                            first_card = None
+                            can_click = True  # 다시 클릭 가능
                             if matches_found == 5:
                                 screen.fill(WHITE)
                                 congrats_text = FONT.render('Congrats!!!', True, RED)
                                 screen.blit(congrats_text, (WIDTH//2 - congrats_text.get_width()//2, HEIGHT//2 - congrats_text.get_height()//2))
                                 pygame.display.flip()
-                                time.sleep(2)
+                                time.sleep(1)
                                 main()
                         else:
-                            time.sleep(1)
+                            pygame.display.flip()
+                            time.sleep(1) #1초간의 확인 딜레이.
                             revealed[first_card] = False
                             revealed[card_index] = False
-                        first_card = None
+                            drawBoard(deck, revealed)
+                            first_card = None
+                            can_click = True  # 다시 클릭 가능
 
     pygame.quit()
 
 if __name__ == "__main__":
     main()
+
+
